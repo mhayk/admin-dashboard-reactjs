9b3ae3d504240215b854219ece13ae42c9a9079f
Mhayk Whandson <hi@mhayk.com>
2019-10-21 13:02:05 +0100

    Route 404(not found) has been configurated.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import Login from './components/Login';
+import NotFound from './components/404';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Switch>
         <Route exact path="/"><Home/></Route>
         <Route exact path="/login"><Login/></Route>
+        <Route path="*"><NotFound/></Route>
       </Switch>
     </Router>
   );
