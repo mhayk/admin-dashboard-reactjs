c805e3f449dd73e8cb469328f1e2ed34eb3b0ab0
Mhayk Whandson <hi@mhayk.com>
2019-10-21 13:12:07 +0100

    NotFound component has been configurated with ReactBodyMovin.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/components/404.js
+++ src/components/404.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import ReactBodyMovin from 'react-bodymovin';
+import NotFoundAnimation from '../NotFound.json';
 
 export default function NotFound() {
+  const bodyMovinOptions = {
+    loop: true,
+    autoplay: true,
+    prerender: true,
+    animationData: NotFoundAnimation
+  }
   return (
-    <div>404</div>
+    <div>
+      <ReactBodyMovin options={bodyMovinOptions} />
+    </div>
   );
 }
